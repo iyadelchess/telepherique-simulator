20c76c8d786a6ee7a22420bc06073ed11e657eb4
Iyad <iyadelchess@gmail.com>
2026-04-08 06:10:25 +0200

    update class cabine definition

--- Cabine.h
+++ Cabine.h
@@ -1,22 +1,38 @@
 #ifndef CABINE_H
 #define CABINE_H
+#include <string>
+using namespace std;
 
+enum EtatCabine {
+    ARRET,
+    EN_MOUVEMENT,
+    EN_PANNE
+};
+enum Portes{
+   OUVERTES,
+   FERMES,
+   ENDOMMAGE
+};
 class Cabine{
+
     private:
+       string modele;
        double position;
        double vitesse;
     public:
        bool  estPropre;
        const int capacité ; 
-       bool  estEnMarche;
-       bool  estEnPanne;
+       bool  porteOuverte;
        Cabine();
 
        void getPosition()const;
        void accelerer();
        void arreter();
        void ralentir();
-       
+       void nettoyer();
+       void declarerEnPanne();
+       void fermerPortes();
+       void ouvrirPortes();
 
 };
 
